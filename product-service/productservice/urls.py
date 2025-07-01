62d6da078a035faa60cc918b4875348a922de5a3
Elauriche Nickson <m.toho@ecoles-epsi.net>
2025-07-01 13:59:12 +0200

    refactor: remove unused import statements from urls.py

--- product-service/productservice/urls.py
+++ product-service/productservice/urls.py
@@ -21,8 +21,6 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework import permissions
 
-from product.views import ProductListCreateView, ProductDetailView
-
 schema_view = get_schema_view(
    openapi.Info(
       title="Product Service API",
